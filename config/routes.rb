09ee22c5149930675dc828adffe873f6da87a29e
Gia Jennings <gjennings.co@gmail.com>
2021-07-02 19:46:22 -0400

    Update styling for home page, create signup form

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :appointments
   resources :doctors
   resources :users
+  root "sessions#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
