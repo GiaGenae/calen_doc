32a1099ca1b6c12e033cecba21736c2e557427e4
Gia Jennings <gjennings.co@gmail.com>
2021-07-03 15:13:42 -0400

    Buid out login page and action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,25 @@ class SessionsController < ApplicationController
 
     end
 
+    def new 
+
+    end
+
+    def create
+        @user = User.find_by(email: params[:user][:email])
+        # if @user && @user.authenticate(params[:user][:password])
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Invalid credentials."
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
     has_many :doctors, through: :appointments
 
     has_secure_password
+
+    validates :email, uniqueness: true, presence: true
 end
