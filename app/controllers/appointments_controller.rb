262d5fcfd42b3d53ef0c65577585da2cfefdddec
Gia Jennings <gjennings.co@gmail.com>
2021-07-04 16:18:43 -0400

    Build out index method and view

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,7 @@
 class AppointmentsController < ApplicationController
 
+    before_action :set_appointment, only:[:show, :edit, :update]
+    before_action :redirect_if_not_logged_in
     def new
         @appointment = Appointment.new
         @appointment.build_doctor
@@ -11,13 +13,27 @@ class AppointmentsController < ApplicationController
         if @appointment.save    
             redirect_to appointment_path(@appointment)
         else
+            @appointment.build_doctor
             render :new
         end
     end
+
+    def index
+        @appointments = Appointment.all
+    end
+
+    def show
+    end
+
 
     private
 
     def appointment_params
         params.require(:appointment).permit(:date, :time, :notes, :user_id, :doctor_id, doctor_attributes: [:first_name, :last_name, :email, :specialty])
+    end
+
+    def set_appointment
+        @appointment = Appointment.find_by(params[:id])
+        redirect_to appointments_path if !@appointment
     end
 end
