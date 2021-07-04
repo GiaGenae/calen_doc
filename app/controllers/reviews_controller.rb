6a1a94f48511b9b036e9b60447ca3b278e65d179
Gia Jennings <gjennings.co@gmail.com>
2021-07-04 18:17:59 -0400

    Create new doctor reviews page& fix associaion

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+
+    def new 
+        @doctor = Doctor.find_by_id(params[:doctor_id])
+        @doctor.reviews.build
+    end
+
+    def index
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
 
   resources :reviews
   resources :appointments
-  resources :doctors
+  resources :doctors do
+    resources :reviews, only:[:new, :index]
+  end
   resources :users
   root "sessions#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
