ab47227c00d851317daa6b05555321dd61a7f90a
Gia Jennings <gjennings.co@gmail.com>
2021-07-04 14:37:46 -0400

    Update new appointment form and functionality

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,9 +2,22 @@ class AppointmentsController < ApplicationController
 
     def new
         @appointment = Appointment.new
+        @appointment.build_doctor
     end
 
-    def doctor_id
+    def create
+        @appointment = Appointment.new(appointment_params)
+        @appointment.user_id = session[:user_id]
+        if @appointment.save    
+            redirect_to appointment_path(@appointment)
+        else
+            render :new
+        end
+    end
 
+    private
+
+    def appointment_params
+        params.require(:appointment).permit(:date, :time, :notes, :user_id, :doctor_id, doctor_attributes: [:first_name, :last_name, :email, :specialty])
     end
 end
