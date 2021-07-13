83b4145dc1bf96a58761d53d1f4b890a5f6660f0
Gia Jennings <gjennings.co@gmail.com>
2021-07-12 20:10:36 -0400

    Remove styling for appointments index page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,30 @@ class ReviewsController < ApplicationController
         @review = @doctor.reviews.build
     end
 
+    def create
+        @review = current_user.reviews.build(review_params)
+        
+        if @review.save
+            redirect_to review_path(@review)
+        else
+            render :new
+        end
+    end
+
+
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+
     def index
 
     end
+
+    def review_params
+        params.require(:review).permit(:doctor_id, :rating)
+    end
+
+    private
+
+
 end
