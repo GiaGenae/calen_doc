00831e5f0d1b5f2aab6327fc5e8700c2b943321a
Gia Jennings <gjennings.co@gmail.com>
2021-07-13 23:42:34 -0400

    Add errors for forms

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,17 @@ class Appointment < ApplicationRecord
     belongs_to :doctor
 
     accepts_nested_attributes_for :doctor
+
+    validates :date, inclusion: { in: (Date.today..Date.today+1.years)}
+    validate :no_duplicate
+
+    def doctor_attributes(attributes)
+        doctor = Doctor.find_or_create_by(attributes) if !attributes['first_name'].empty?
+    end
+
+    def no_duplicate
+        if Appointment.find_by(date: date, doctor_id: doctor_id)
+            errors.add(:date, "already has an duplicate appointment. Please review.")
+        end
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
 
-  validates :rating, presence: true, 
-    numericality: {only_integer: true, 
-    greater_than_or_equal_to: 0, 
-    less_than_or_equal_to: 5}
+  validates :rating, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
+  validates :doctor, uniqueness: { scope: :user, message: "has already been reviewed by you."}
 end
