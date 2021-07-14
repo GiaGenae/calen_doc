f6a7906998e383728d3e07543f9258b9678b79d0
Gia Jennings <gjennings.co@gmail.com>
2021-07-14 00:25:08 -0400

    Add google omniauth gem

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Omniauth for third party authorization
 gem 'omniauth'
-gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
 gem 'omniauth-rails_csrf_protection'
 
 group :development, :test do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,11 @@ class SessionsController < ApplicationController
     end
 
     def create
+        if params[:provider] == 'google_oauth2'
+            @user = User.create_by_google_omniauth(auth)
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+
         @user = User.find_by(email: params[:user][:email])
         # if @user && @user.authenticate(params[:user][:password])
         if @user.try(:authenticate, params[:user][:password])
